3913b87e3b30ec4d580024a61d9d4802c9830a89
Govind <42733570+realzenfox@users.noreply.github.com>
2022-03-14 00:08:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+function rollDice () {
+
+      var randomNumber1 = Math.floor(Math.random() * 6) + 1; // gives random number between 1-6
+
+      var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png to dice6.png
+
+      var randomImageSource = "images/" + randomDiceImage; //images/dice1.png to images/dice6.png
+
+      var image1 = document.querySelectorAll("img")[0];
+
+      image1.setAttribute("src", randomImageSource);
+
+
+      var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+      var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+      var image2= document.querySelectorAll("img")[1];
+
+      image2.setAttribute("src", randomImageSource2);
+
+
+      //if player 1 wins
+      if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 1 Wins 🚩";
+      }
+      else if(randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins 🚩";
+      }
+      else {
+        document.querySelector("h1").innerHTML = "Draw";
+      }
+
+}
